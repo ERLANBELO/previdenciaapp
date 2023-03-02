bc0de5148ae30e1d222448d1e50a86cae98d74e9
erlanbelo <Erlan.silva@etec.sp.gov.br>
2023-03-01 22:13:59 -0300

    segundo commit erlan

--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -4,13 +4,32 @@ import { HomePage } from './home.page';
 
 const routes: Routes = [
   {
-    path: '',
+    path: 'tabs',
     component: HomePage,
+    children: [
+      {
+        path: 'solicitacao',
+        loadChildren: () => import('../solicitacao/solicitacao.module').then( m => m.SolicitacaoPageModule)
+      },
+      {
+        path: 'notificacao',
+        loadChildren: () => import('../notificacao/notificacao.module').then( m => m.NotificacaoPageModule)
+      },
+      {
+        path: '',
+        redirectTo: '/tabs/solicitacao',
+        pathMatch: 'full'
+      }
+    ]
+  },
+  {
+    path: '',
+    redirectTo: '/tabs/solicitacao',
+    pathMatch: 'full'
   }
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
 })
 export class HomePageRoutingModule {}
